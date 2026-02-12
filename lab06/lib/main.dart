8354a538711dcc66018ebfb110e8b816e29fd4e9
sergioadahirsalvadorpadilla-gif <sergioadahirsalvadorpadilla@gmail.com>
2026-02-12 09:50:45 -0800

    Trabajando con sensores 2

--- lab06/lib/main.dart
+++ lab06/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sensors_plus/sensors_plus.dart'; // Asegúrate de tener esto en pubspec.yaml
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Cambiamos el nombre a HomePage para que coincida con lo que llama MyApp
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -26,13 +26,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // Inicializamos las variables
+  
   double x = 0.0, y = 0.0, z = 0.0;
 
   @override
   void initState() {
     super.initState();
-    // Suscripción correcta al sensor
+    
     accelerometerEvents.listen((AccelerometerEvent event) {
       setState(() {
         x = event.x;
@@ -42,7 +42,7 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  // El método build DEBE estar dentro de _HomePageState para ver x, y, z
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,7 +54,7 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // toStringAsFixed(2) limita los decimales para que no "salten" en pantalla
+          
             Text("Eje X: ${x.toStringAsFixed(2)}", style: const TextStyle(fontSize: 20)),
             Text("Eje Y: ${y.toStringAsFixed(2)}", style: const TextStyle(fontSize: 20)),
             Text("Eje Z: ${z.toStringAsFixed(2)}", style: const TextStyle(fontSize: 20)),
